2ff1840dbb3440c8763cc5dc8a166f55ca0120a1
dondycles <johnrod.dondoyano@gmail.com>
2026-01-09 14:46:23 +0800

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
+  adapter: vercel(),
 });
